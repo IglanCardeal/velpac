ab3d2032764ff969cbb2cd1193cc33d24086b902
Iglan Cardeal <cmtcardeal@outlook.com>
2019-07-07 17:32:10 -0300

    Adicionando o conteudo e ajustando style do componentes.

--- app.js
+++ app.js
@@ -6,16 +6,25 @@ const PORT = process.env.PORT || 3000;
 import express from "express";
 import path from "path";
 
-// Rotas
-import home from "./backend/routes/home";
-
 const app = express();
+const router = express.Router();
 
 app.set("view engine", "ejs");
 app.set("views", "./frontend/");
 app.use(express.static(path.join(__dirname, "frontend", "public")));
 
-app.use(home);
+app.use(
+  router.get("/", (req, res) => {
+    res.render("views/index");
+  })
+);
+
+app.use(
+  router.get("/pdf/documentacao", (req, res) => {
+    const path = __dirname + "/frontend/public/documents/documentacao.pdf";
+    res.sendFile(path);
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`
@@ -23,3 +32,4 @@ app.listen(PORT, () => {
 Servidor ONLINE. Porta: ${PORT}
 `);
 });
+
